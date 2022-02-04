a1c4ca3ae7f1314dd46581ebd2b23fedc8736d0a
leonardo morales <moralesparadaleonardo@gmail.com>
2022-02-04 17:23:19 -0500

    agregamos useState al componente TodoSearch

--- mitodo/src/TodoSearch.js
+++ mitodo/src/TodoSearch.js
@@ -1,16 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import './TodoSearch.css'
 
 function TodoSearch () {
 
+    const [searchValue, setSearchValue] = useState('')
+
 const onSearchValue = (event) => {
-  console.log(event.target.value)
+    console.log(event.target.value);
+  setSearchValue(event.target.value);
 }
 
     return(
         <input 
         className="TodoSearch" 
         placeholder="cebolla"
+        value={searchValue}
         onChange={onSearchValue}
         />
     )
