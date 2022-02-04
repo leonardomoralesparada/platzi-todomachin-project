26cd1cac9e43d8cf81db57974975a4cb5b88ada5
leonardo morales <moralesparadaleonardo@gmail.com>
2022-02-04 17:04:22 -0500

    introducimos los eventos a los componentes de CreateTodoButton y TodoSearch

--- mitodo/src/TodoSearch.js
+++ mitodo/src/TodoSearch.js
@@ -2,8 +2,17 @@ import React from "react";
 import './TodoSearch.css'
 
 function TodoSearch () {
+
+const onSearchValue = (event) => {
+  console.log(event.target.value)
+}
+
     return(
-        <input className="TodoSearch" placeholder="cebolla"/>
+        <input 
+        className="TodoSearch" 
+        placeholder="cebolla"
+        onChange={onSearchValue}
+        />
     )
 }
 
