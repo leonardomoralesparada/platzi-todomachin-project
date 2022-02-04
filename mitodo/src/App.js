85d60be42a1d5c22e511129f675c983054c46b58
leonardo morales <moralesparadaleonardo@gmail.com>
2022-02-04 15:45:30 -0500

    creando componentes y agregandoles elementos

---
+++ mitodo/src/App.js
@@ -0,0 +1,37 @@
+import React, { Fragment } from "react";
+import TodoCounter from "./TodoCounter";
+import TodoSearch from "./TodoSearch";
+import CreateTodoButton from "./CreateTodoButton";
+import TodoList from "./TodoList";
+import TodoItem from "./TodoItem";
+//import './App.css';
+
+const todos = [
+  {text:'cortar cebolla' ,completed: false},
+  {text:'tomar el curso de intro a react', completed: false},
+  {text:'llorar con la llorona', completed: false},
+]
+
+
+function App() {
+  return (
+
+  <Fragment>                   
+    <TodoCounter/>
+
+    <TodoSearch/>
+    
+
+    <TodoList>
+    {todos.map(todo =>(
+        <TodoItem key={todo.text} text={todo.text}/>
+    ))}
+    </TodoList>
+
+    <CreateTodoButton/>
+  
+  </Fragment>   
+  );
+}
+
+export default App;
---
+++ mitodo/src/TodoItem.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function TodoItem (props) {
+    return(
+        <li>
+            <span>c</span>
+
+            <p>{props.text}</p>
+            
+            <span>x</span>
+        </li>
+
+    )
+}
+
+export default TodoItem;
